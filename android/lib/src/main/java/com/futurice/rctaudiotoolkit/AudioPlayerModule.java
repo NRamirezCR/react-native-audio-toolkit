cbea8b87902a59fda54ab69cb42755475890d203
KalebMatthews <kaleb@shontz.net>
2018-02-22 14:05:49 -0500

    Test non async call for prepare.

--- android/lib/src/main/java/com/futurice/rctaudiotoolkit/AudioPlayerModule.java
+++ android/lib/src/main/java/com/futurice/rctaudiotoolkit/AudioPlayerModule.java
@@ -248,14 +248,6 @@ public class AudioPlayerModule extends ReactContextBaseJavaModule implements Med
         player.setOnInfoListener(this);
         player.setOnCompletionListener(this);
         player.setOnSeekCompleteListener(this);
-        player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() { // Async preparing, so we need to run the callback after preparing has finished
-
-            @Override
-            public void onPrepared(MediaPlayer player) {
-                callback.invoke();
-            }
-
-        });
 
         this.playerPool.put(playerId, player);
 
@@ -277,7 +269,8 @@ public class AudioPlayerModule extends ReactContextBaseJavaModule implements Med
         this.playerContinueInBackground.put(playerId, continueInBackground);
 
         try {
-            player.prepareAsync();
+            player.prepare();
+            callback.Invoke(null, getInfo(player))
         } catch (Exception e) {
             callback.invoke(errObj("prepare", e.toString()));
         }
